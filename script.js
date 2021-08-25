abc1c03ae195d42ea697822ddf15f4b8f7c14847
Gopi <mail2gvrm@gmail.com>
2021-08-25 23:09:22 +0530

    completed

--- script.js
+++ script.js
@@ -32,52 +32,53 @@ document.getElementById("remaining").innerHTML=chg;
 
   switch (true){
 
-    case(chg>=2000) :
+    case(chg>=2000) :{
     let x= Math.floor(chg/2000);
     Two000.innerHTML=`:  ${x}`;
     chg= (chg-(x*2000));
+  }
 
-
-    case(chg>=500) :
-    let y= Math.floor(chg/500);
-    Five00.innerHTML=`:  ${y}`;
-    chg=(chg-(y*500));
-
-    case(chg>=200) :
-    let z= Math.floor(chg/200);
-    Two00.innerHTML=`:  ${z}`;
-    chg=(chg-(z*200));
-
-    case(chg>=100) :
-    let a= Math.floor(chg/100);
-    One00.innerHTML=`:  ${a}`;
-    chg=(chg-(a*100));
-    
-    case(chg>=20) :
-    let b= Math.floor(chg/20);
-    Twenty.innerHTML=`:  ${b}`;
-    chg=(chg-(b*20));
-
-    case(chg>=10) :
-    let c= Math.floor(chg/10);
-    Ten.innerHTML=`:  ${c}`;
-    chg=(chg-(c*10));
-
-    case(chg>=5) :
-    let d= Math.floor(chg/5);
-    Five.innerHTML=`:  ${d}`;
-    chg=(chg-(d*5));
-
-    case(chg>=2) :
-    let e= Math.floor(chg/2);
-    Two.innerHTML=`:  ${e}`;
-    chg=(chg-(e*2));
-    
-    case(chg>=1) :
-    let f= Math.floor(chg/1);
-    One.innerHTML=`:  ${f}`;
-    chg=(chg-(f*2));
+    case(chg>=500) :{
+    let x= Math.floor(chg/500);
+    Five00.innerHTML=`:  ${x}`;
+    chg=(chg-(x*500));
+  }
+    case(chg>=200) :{
+    let x= Math.floor(chg/200);
+    Two00.innerHTML=`:  ${x}`;
+    chg=(chg-(x*200));
+  }
+    case(chg>=100) :{
+    let x= Math.floor(chg/100);
+    One00.innerHTML=`:  ${x}`;
+    chg=(chg-(x*100));
+    }
+    case(chg>=20) :{
+    let x= Math.floor(chg/20);
+    Twenty.innerHTML=`:  ${x}`;
+    chg=(chg-(x*20));
+  }
+    case(chg>=10) :{
+    let x= Math.floor(chg/10);
+    Ten.innerHTML=`:  ${x}`;
+    chg=(chg-(x*10));
+  }
+    case(chg>=5) :{
+    let x= Math.floor(chg/5);
+    Five.innerHTML=`:  ${x}`;
+    chg=(chg-(x*5));
+  }
+    case(chg>=2) :{
+    let x= Math.floor(chg/2);
+    Two.innerHTML=`:  ${x}`;
+    chg=(chg-(x*2));
+    }
+    case(chg>=1) :{
+    let x= Math.floor(chg/1);
+    One.innerHTML=`:  ${x}`;
+    chg=(chg-(x*2));
     break;
+  }
   }
 }
 
